9e4118e47906ecb0ce7f6e384fedd467ce03ac98
SeanAtho <sean@atho.com.au>
2023-07-02 12:28:08 +1000

    Updated Comments for Program

--- Node.cpp
+++ Node.cpp
@@ -1,47 +1,77 @@
 #include "Node.h"
 #include <iostream>
 
-
+/*
+ * Constructor for the Node class. Initializes the node with row, column and distance traveled.
+ * All initialization is done in the member initializer list.
+ */
 Node::Node(int row, int col, int dist_traveled) 
 : row(row), col(col), dist_traveled(dist_traveled)
 {
-    // Constructor implementation
 }
 
+
+/*
+ * Destructor for the Node class. 
+ * No dynamically allocated resources are present, so no cleanup is required.
+ */
 Node::~Node(){
-    // Destructor implementation
 }
 
+/*
+ * Getter for the 'row' field of the node.
+ */
 int Node::getRow(){
     return row;
 }
 
+/*
+ * Getter for the 'col' field of the node.
+ */
 int Node::getCol(){
     return col;
 }
 
+/*
+ * Getter for the 'dist_traveled' field of the node.
+ */
 int Node::getDistanceTraveled(){
     return dist_traveled;
 }
 
+/*
+ * Setter for the 'dist_traveled' field of the node.
+ */
 void Node::setDistanceTraveled(int dist_traveled)
 {
     this->dist_traveled = dist_traveled;
 }
 
+/*
+ * Computes and returns the estimated distance from the current node to the goal node.
+ */
 int Node::getEstimatedDist2Goal(Node* goal){
+    /*
+     * If the goal node is null, throw an exception.
+     */
     if (goal == nullptr) {
         throw std::invalid_argument("Goal node is a null pointer.");
     }
-    // Calculate the Manhattan distance between this node and the goal node
+    /*
+     * Calculate the Manhattan distance between this node and the goal node.
+     * Return the estimated distance by adding the current distance traveled to the Manhattan distance.
+     */
     int manhattanDistance = abs(row - goal->getRow()) + abs(col - goal->getCol());
-
-    // Return the estimated distance by adding the current distance traveled to the Manhattan distance
     return dist_traveled + manhattanDistance;
 }
     
 //--------------------------------                             
 
+
+/*
+ * Checks if the current node is the goal node.
+ * Returns true if the row and column of the current node match the row and column of the goal node.
+ */
 bool Node::isGoal(Node* goal) {
     return (row == goal->getRow()) && (col == goal->getCol());
 }
--- PathSolver.cpp
+++ PathSolver.cpp
@@ -13,41 +13,50 @@ PathSolver::~PathSolver(){
     delete nodesExplored;
 }
 
+// Method: forwardSearch
+// This method performs the pathfinding search on the environment using the A* algorithm.
 void PathSolver::forwardSearch(Env env) {
-    // Get the start and goal positions from the environment
+    // Define Node pointers for start and goal nodes.
     Node* start = nullptr;
     Node* goal = nullptr;
 
+    // Iterate over the environment grid to find the start and goal positions.
     for (int i = 0; i < ENV_DIM; i++) {
         for (int j = 0; j < ENV_DIM; j++) {
+            // Create a new start node if the current position is the start symbol.
             if (env[i][j] == SYMBOL_START) {
                 start = new Node(i, j, 0);
-            } else if (env[i][j] == SYMBOL_GOAL) {
+            } 
+            // Create a new goal node if the current position is the goal symbol.
+            else if (env[i][j] == SYMBOL_GOAL) {
                 goal = new Node(i, j, 0);
             }
         }
     }
 
-    // Create the open list and closed list
+    // Initialize the open and closed lists for the A* algorithm.
     NodeList openList;
     NodeList closedList;
 
-    // Add the start position to the open list
+    // Add the start node to the open list to begin the algorithm.
     openList.addElement(start);
 
+    // Initialize a boolean to track if the goal has been reached.
     bool goalReached = false;
 
+    // Execute the main loop of the A* algorithm until the open list is empty or the goal is reached.
     while (openList.getLength() > 0 && !goalReached) {
-        // Select the node with the smallest estimated distance from the open list
+        // The current node is the node from the open list with the smallest estimated distance to the goal.
         Node* current = nullptr;
         int smallestDistance = INT_MAX;
 
-        // Iterate over the nodes in the open list and find the node with the smallest estimated distance
+        // Find the node with the smallest estimated distance in the open list.
         for (int i = 0; i < openList.getLength(); i++) {
             Node* node = openList.getNode(i);
             if (node != nullptr) {
                 int estimatedDistance = node->getEstimatedDist2Goal(goal);
 
+                // Update the current node if the estimated distance is smaller.
                 if (estimatedDistance < smallestDistance) {
                     smallestDistance = estimatedDistance;
                     current = node;
@@ -57,45 +66,43 @@ void PathSolver::forwardSearch(Env env) {
             }
         }
 
-        // Remove the current node from the open list
+        // Move the current node from the open list to the closed list.
         openList.removeElement(current);
-
-        // Add the current node to the closed list
         closedList.addElement(current);
 
-        // Check if the current node is the goal
+        // Check if the current node is the goal, and update the boolean accordingly.
         bool isGoal = current->isGoal(goal);
         goalReached = isGoal;
 
-        // Explore neighboring positions
+        // If the current node is not the goal, explore its neighbors.
         int row = current->getRow();
         int col = current->getCol();
 
-        // Check neighboring positions: up, down, left, right
+        // The direction vectors represent up, down, left, right.
         int dx[4] = {0, 0, -1, 1};
         int dy[4] = {-1, 1, 0, 0};
 
+        // Explore each neighboring position.
         for (int i = 0; i < 4; i++) {
             int newRow = row + dy[i];
             int newCol = col + dx[i];
 
-            // Check if the new position is valid and not a wall
+            // Verify that the new position is within the environment and not a wall.
             bool validPosition = newRow >= 0 && newRow < ENV_DIM && newCol >= 0 && newCol < ENV_DIM &&
                                  env[newRow][newCol] != SYMBOL_WALL;
 
             if (validPosition) {
-                // Create a new node for the neighboring position
+                // Create a new neighbor node.
                 Node* neighbor = new Node(newRow, newCol, current->getDistanceTraveled() + 1);
 
-                // Check if the neighbor is already in the closed list
+                // If the neighbor node is not in the closed list, consider it for the open list.
                 bool inClosedList = closedList.containsNode(neighbor);
 
                 if (!inClosedList) {
-                    // Check if the neighbor is already in the open list
                     bool inOpenList = openList.containsNode(neighbor);
 
+                    // Add the neighbor node to the open list if it's not already there.
                     if (!inOpenList) {
-                        // Add the neighbor to the open list
                         openList.addElement(neighbor);
                     } else {
                         delete neighbor;
@@ -107,62 +114,78 @@ void PathSolver::forwardSearch(Env env) {
         }
     }
 
-    // Store the explored positions in nodesExplored
+    // Save the nodes that have been explored (those in the closed list).
     delete nodesExplored;
     nodesExplored = new NodeList(closedList);
 
-    // Clean up
+    // Clean up the dynamically allocated memory.
     delete start;
     delete goal;
 }
 
 
 
-
+// Method: getNodesExplored
+// This method returns a deep copy of the nodesExplored list.
 NodeList* PathSolver::getNodesExplored() {
-    // Create a deep copy of the nodesExplored list
+    // Create a new NodeList instance as a copy of the nodesExplored.
     NodeList* copyList = new NodeList(*nodesExplored);
+    
+    // Return the copy of nodesExplored.
     return copyList;
 }
 
+// Function: getPath
+// This function finds the shortest path from start to goal using the explored nodes from forwardSearch function.
 NodeList* PathSolver::getPath(Env env) {
+    // Initialize a new NodeList object to store the path.
     NodeList* path = new NodeList();
+    
+    // Initialize a Node pointer to store the goal node.
     Node* goalNode = nullptr;
 
-    // Find the goal node in the explored nodes
+    // Iterate over the explored nodes to find the goal node.
     for(int i = 0; i < nodesExplored->getLength(); i++) {
         Node* node = nodesExplored->getNode(i);
         if(env[node->getRow()][node->getCol()] == SYMBOL_GOAL) {
+            // Set goalNode to the current node if it is the goal node.
             goalNode = node;
         }
     }
 
+    // If a goal node was found
     if(goalNode != nullptr) {
+        // Start with the goal node and work backwards to find the path.
         Node* currentNode = goalNode;
+        
+        // Initialize a temporary NodeList object to store the path in reverse.
         NodeList* tempPath = new NodeList();
 
-        // Add goal node to tempPath
+        // Add the goal node to tempPath.
         tempPath->addElement(currentNode);
 
-        while(currentNode->getDistanceTraveled() > 0) { // until we reach the start node
-            // Check neighboring positions: up, right, down, left
+        // Continue until the start node (whose distance traveled is 0) is reached.
+        while(currentNode->getDistanceTraveled() > 0) {
+            // Define the direction vectors for the neighboring positions (up, right, down, left).
             int dx[4] = {0, 1, 0, -1};
             int dy[4] = {-1, 0, 1, 0};
             
             bool foundValidNeighbor = false;
 
+            // Iterate over the neighboring positions.
             for (int i = 0; i < 4 && !foundValidNeighbor; i++) {
                 int newRow = currentNode->getRow() + dy[i];
                 int newCol = currentNode->getCol() + dx[i];
 
-                // Check if the new position is valid
+                // Check if the new position is within the bounds of the environment.
                 bool validPosition = newRow >= 0 && newRow < ENV_DIM && newCol >= 0 && newCol < ENV_DIM;
 
                 if (validPosition) {
+                    // Iterate over the explored nodes to find a valid neighbor.
                     for(int j = 0; j < nodesExplored->getLength() && !foundValidNeighbor; j++) {
                         Node* node = nodesExplored->getNode(j);
 
-                        // If this node is a neighbor of the current node and its distance traveled is one less than the current node's
+                        // Check if this node is a neighbor of the current node and its distance traveled is one less than the current node's.
                         if(node->getRow() == newRow && node->getCol() == newCol && node->getDistanceTraveled() == currentNode->getDistanceTraveled() - 1) {
                             currentNode = node;
                             foundValidNeighbor = true;
@@ -171,18 +194,21 @@ NodeList* PathSolver::getPath(Env env) {
                 }
             }
             if (foundValidNeighbor) {
-                // Only add the node to the path if a valid neighbor was found
+                // Only add the node to the path if a valid neighbor was found.
                 tempPath->addElement(currentNode);
             }
         }
 
-        // Reorder tempPath from start to finish and assign it to path
+        // Reorder tempPath from start to finish and assign it to path.
         for(int i = tempPath->getLength() - 1; i >= 0; i--) {
             path->addElement(tempPath->getNode(i));
         }
+        
+        // Delete tempPath as it is no longer needed.
         delete tempPath;
     }
 
+    // Return the path.
     return path;
 }
 
